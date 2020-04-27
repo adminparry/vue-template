a1931571de2c0955e6cc2bceec1e469438d8bf76
pparry <1170015014@qq.com>
2020-04-27 12:14:34 +0800

    default hash mode

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ Vue.use(Router)
 
 // expose Router Instance 
 const RouterInstance = new Router({
-  mode: 'history',
+  mode: 'hash',
   scrollBehavior: () => ({ y: 0 }),
   base: process.env.BASE_URL,
   routes: routers
